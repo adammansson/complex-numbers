07ffb986f3ce591f72e44708d8a0592c669e1159
adammansson <adam.s.mansson@gmail.com>
2022-01-20 20:35:41 +0100

    update main

--- src/complex/Main.java
+++ src/complex/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         ComplexNumber a = new ComplexNumber(1, 1);
-        ComplexNumber b = new ComplexNumber(2, 2);
-        ComplexNumber c = new ComplexNumber(-1, 2);
+        ComplexNumber b = new ComplexNumber(-1, 2);
 
-        ComplexPlane p = new ComplexPlane(7, 7);
+        ComplexPlane p = new ComplexPlane(11, 11);
         p.add(a);
+        p.add(b);
+        p.add(a.conj());
+        p.add(a.multiply(b));
 
+        p.draw();
     }
 }
